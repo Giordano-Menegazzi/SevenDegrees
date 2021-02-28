b01ef88daa7978bc7c0a900d43c8a69a854aebb2
GioLisa <minigazzi@hotmail.com>
2021-02-28 13:17:06 +0100

    Initial Commit

---
+++ SevenDegrees/SevenDegreesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SevenDegreesApp.swift
+//  SevenDegrees
+//
+//  Created by Giordano Menegazzi on 28/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct SevenDegreesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
